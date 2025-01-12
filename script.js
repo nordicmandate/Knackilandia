a77995101c7991804883a799a7efe3cb0748606f
nordicmandate <alvarwikstrom@icloud.com>
2025-01-12 23:08:12 +0100

    updated code

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-const targetDate = new Date("June 20, 2025 00:00:00").getTime();
-const countdownInterval = setInterval(function() {
-    const now = new Date().getTime();
-    const remainingTime = targetDate - now;
-    const days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+// Countdown timer logic
+const countdownDate = new Date("June 20, 2025 00:00:00").getTime();
 
-    document.getElementById("countdown-timer").innerHTML =
+const countdownFunction = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML = 
         days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
-    if (remainingTime <= 0) {
-        clearInterval(countdownInterval);
-        document.getElementById("countdown-timer").innerHTML = "Midsummer is here!";
+    if (distance < 0) {
+        clearInterval(countdownFunction);
+        document.getElementById("countdown").innerHTML = "Midsummer has arrived!";
     }
 }, 1000);
