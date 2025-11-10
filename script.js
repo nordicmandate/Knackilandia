2f3cc1158b23892a45c3bff594fd11658970df15
Alvar <alvarwikstrom@icloud.com>
2025-11-10 16:43:50 +0100

    new line

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Countdown timer logic
-const countdownDate = new Date("June 20, 2025 00:00:00").getTime();
+const countdownDate = new Date("November 14, 2025 18:00:00").getTime();
 
 const countdownFunction = setInterval(function() {
     const now = new Date().getTime();
